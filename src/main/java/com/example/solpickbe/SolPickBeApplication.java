7b35fd29435e692dd4e36c67b6a68390dcff70a5
Jinyoung Yang <jy2ang@naver.com>
2025-03-05 16:09:46 +0900

    refactor:application.properties 수정

---
+++ src/main/java/com/example/solpickbe/SolPickBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.solpickbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolPickBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolPickBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solpickbe/SolPickBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solpickbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolPickBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
