9113f0cc508e0b308653616b57ba130eff48e818
ayeooong <ayeong0115@naver.com>
2025-03-18 04:58:48 +0900

    feat: 모든 회원 ID만 조회하는 메서드 추가

--- src/main/java/kr/co/solpick/member/repository/MemberRepository.java
+++ src/main/java/kr/co/solpick/member/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package kr.co.solpick.member.repository;
 
 import kr.co.solpick.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,8 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 //    // 레시픽 사용자 ID로 회원 조회
     Optional<Member> findByRecipickUserId(Integer recipickUserId);
+
+    // 모든 회원 ID만 조회
+    @Query("SELECT m.id FROM Member m")
+    List<Integer> findAllMemberIds();
 }
